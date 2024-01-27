fc8ae0708a7feb67854bb31a5c68c740a9f6f03e
Daalmodi <daalmodi@gmail.com>
2024-01-27 17:55:18 -0500

    firebase medio configurado

---
+++ Neon-Add-Aplication/src/app/nav-bar-main/nav-bar-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar-main.component.html',
+  styleUrl: './nav-bar-main.component.scss'
+})
+export class NavBarMainComponent {
+
+}
